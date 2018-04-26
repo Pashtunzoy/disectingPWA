e19318867f20f6d93c9cc70cebf4fe38e267fd97
pashtunzoy <asifkhannoorzai@gmail.com>
2018-04-26 22:13:10 +1000

    Added the exercise answer

--- client/index.js
+++ client/index.js
@@ -5,6 +5,7 @@ import './index.scss';
 import App from './App';
 import VAPID from './../private/vapid.json';
 
+
 import 'file-loader?name=./img/launcher-icon-1x.png!./img/launcher-icon-1x.png';
 import 'file-loader?name=./img/launcher-icon-2x.png!./img/launcher-icon-2x.png';
 import 'file-loader?name=./img/launcher-icon-4x.png!./img/launcher-icon-4x.png';
@@ -17,6 +18,8 @@ import 'file-loader?name=./apple-touch-icon-120x120.png!./img/apple-touch-icon-1
 import 'file-loader?name=./apple-touch-icon-144x144.png!./img/apple-touch-icon-144x144.png';
 import 'file-loader?name=./apple-touch-icon-152x152.png!./img/apple-touch-icon-152x152.png';
 import 'file-loader?name=./apple-touch-icon-180x180.png!./img/apple-touch-icon-180x180.png';
+
+import 'file-loader?name=web-app-manifest.json!./web-app-manifest.json';
 
 ReactDOM.render((<App />), document.getElementById('root'));
 
