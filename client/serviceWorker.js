1143b38946a3a05b6d96a592ff5562ada571eda7
pashtunzoy <asifkhannoorzai@gmail.com>
2018-05-03 23:16:35 +1000

    Exercise: Added a fallback image functionality to service worker

--- client/serviceWorker.js
+++ client/serviceWorker.js
@@ -1,12 +1,37 @@
 
-let counter = 0;
+const currentCache = 'assets-v1';
+const fallBackImage = 'https://localhost:3100/images/fallback-vegetables.png';
 
 self.addEventListener('install', event => {
-  console.log(`Counter is counting in install: ${++counter}`);
+  event.waitUntil(
+    caches.open(currentCache).then(cache => {
+      cache.add(fallBackImage);
+    })
+  );
 });
+
 self.addEventListener('activate', event => {
   console.log('service worker activated: ', event);
 });
+
 self.addEventListener('fetch', event => {
-  console.log('Fetch Request is made: ', event.request.url);
+  const acceptHeader = event.request.headers.get('accept');
+  const requestUrl = new URL(event.request.url);
+
+  if(acceptHeader.indexOf('image/*') >= 0 &&
+  requestUrl.pathname.indexOf('/images/') === 0) {
+
+    event.respondWith(
+      fetch(event.request.url)
+        .then(res => {
+          if (!res.ok) {
+            return caches.match(fallBackImage, { cacheName: currentCache});
+          } else {
+            return res;
+          }
+        }).catch(err => 
+          caches.match(fallBackImage, { cacheName: currentCache })
+        )
+    );
+  }
 });
