221ea1e292f88fa52b462f5d4626c421f2c078cf
pashtunzoy <asifkhannoorzai@gmail.com>
2018-05-10 22:47:34 +1000

    Finishing up the Web Push exercise

--- client/sw.js
+++ client/sw.js
@@ -168,3 +168,19 @@ self.addEventListener('fetch', event => {
       })
   );
 });
+
+
+// Web PUSH eventListener implementation
+self.addEventListener('push', function (event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Message';
+  const options = {
+    body: 'Yay it works.',
+    // icon: 'images/icon.png',
+    // badge: 'images/badge.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
