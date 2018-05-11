3d1f29a22ee27732585ed8412551a874f9cc7fcc
pashtunzoy <asifkhannoorzai@gmail.com>
2018-05-11 22:16:07 +1000

    Added some extra functionalty to the notification from the slides

--- client/sw.js
+++ client/sw.js
@@ -194,5 +194,19 @@ self.addEventListener('push', function (event) {
 });
 
 
-
-
+self.addEventListener('notificationclick', function (event) {
+  console.log('On notification click: ', event.notification.tag);
+  event.notification.close();
+  // This looks to see if the current is already open and focuses if it is
+  event.waitUntil(self.clients.matchAll({
+    type: 'window'
+  }).then(function (clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url == 'https://localhost:3000/' && 'focus' in client)
+        return client.focus();
+    }
+    if (self.clients.openWindow)
+      return self.clients.openWindow('https://localhost:3000/');
+  }));
+});
